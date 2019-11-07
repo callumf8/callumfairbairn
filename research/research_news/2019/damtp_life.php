cb7aa2fac807734b8d5d95fc7d4d62994404d789
Callum Fairbairn <callumf8@hotmail.co.uk>
2019-11-07 23:29:14 +0000

    Update damtp_life.php

--- research/research_news/2019/damtp_life.php
+++ research/research_news/2019/damtp_life.php
@@ -23,10 +23,6 @@ include_once($path);
 <div class="research-news-article container">
      
 <h2>DAMTP Life - 07/11/19</h2>
-    
-<a href="research/research_news/2019/images/matric.JPG">
-<img src="research/research_news/2019/images/matric.JPG" style="width:100%">
-</a>
   
 <p> Update coming soon...stay posted!</p>
       
