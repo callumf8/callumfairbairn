bf14cf3a76e96c5caa30b9f8151fcde719167394
Callum Fairbairn <callumf8@hotmail.co.uk>
2019-12-11 22:04:28 +0000

    Christmas article

--- cambridge/2019/christmas.php
+++ cambridge/2019/christmas.php
@@ -23,7 +23,21 @@ include_once($path);
 <div class="cambridge-news-article container">
      
 <h2>Merry Christ's-Mas - 11/12/19</h2>
-           
+     
+<a href="cambridge/2019/Images/christmas.jpg">
+<img src="cambridge/2019/Images/christmas.jpg" style="width:80%">
+</a>
+     
+<p>
+It has been a very `Merry Bridgemas' at Christ's college! The festivities in Cambridge start particularly early (25th November to be exact!), so an appropriate amount of carols can be sung and mulled wine consumed before the term ends on the 3rd of December. ALso, the town is host to a range of beautiful carol services - I have been to a couple in college and one in Great St Mary's already. Oh come oh come Emmanuel is officially a banger.
+</p>
+<p>
+The MCR have once done an amazing job with a bunch of Christmas crafts event, a winter wonderland bop and a fantastic christmas dinner! In fact, I have been so admiring of all their hard work I have decided to get involved - starting from next term I will be the Christs College Welfare Officer!
+</p>
+<p> 
+As my  mind turns towards the drive home for Christmas and the New Year I'm buzzing to be headed back to Scotland. However, I'm gonna miss Cambridge and the amazing people here as well! I'm certainly excited about what things lie in store for 2020 and the new decade. After a somewhat tumultuous year of change for me, I'm really happy to have settled in such amn interesting place! Thanks for Michaelmas - bring on Lent term!
+</p> 
+     
 </div>
               
 <!---------------FOOTER----------------------------------->    
