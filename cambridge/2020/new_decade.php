7987c2ad674a78dee159fd5d9264a36d97b22d86
Callum Fairbairn <callumf8@hotmail.co.uk>
2020-03-28 11:36:25 +0000

    Update new_decade.php
    Adding to new decadew article

--- cambridge/2020/new_decade.php
+++ cambridge/2020/new_decade.php
@@ -22,9 +22,10 @@ include_once($path);
 <!-----------------RESEARCH NEWS------------------------------->     
 <div class="cambridge-news-article container">
      
-<h2>New Decade - 13/03/20</h2>
+<h2>New Decade - 28/03/20</h2>
      
-<p>It seems like yesterday that it was 2010.</p>
+<p>It's been a long time since my last update - there is a real shortage of this valuable resource called...free time...in Cambridge! It's been a turbulent year so far and we are only into March. Brexit has officially happened and for most people in the academic community it represents a big new challenege to collaborative and funding opportunities. Nonetheless, I have had a good time progressing with my work in Cambridge and my little grey cells are getting pushed every step of the way. Outside of work it started off as a fab Lent term. We celebrated a fantastic Burn's night in College as I delivered the address to a haggis and joined in with the ceilidhing - a little bit of home!
+</p>
      
 </div>
               
