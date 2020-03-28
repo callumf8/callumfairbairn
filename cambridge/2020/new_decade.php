4cbc24eb2ce105a1db1ccde2d4f0d28303aa82c6
Callum Fairbairn <callumf8@hotmail.co.uk>
2020-03-28 12:30:47 +0000

    Update new_decade.php

--- cambridge/2020/new_decade.php
+++ cambridge/2020/new_decade.php
@@ -24,7 +24,30 @@ include_once($path);
      
 <h2>New Decade - 28/03/20</h2>
      
-<p>It's been a long time since my last update - there is a real shortage of this valuable resource called...free time...in Cambridge! It's been a turbulent year so far and we are only into March. Brexit has officially happened and for most people in the academic community it represents a big new challenege to collaborative and funding opportunities. Nonetheless, I have had a good time progressing with my work in Cambridge and my little grey cells are getting pushed every step of the way. Outside of work it started off as a fab Lent term. We celebrated a fantastic Burn's night in College as I delivered the address to a haggis and joined in with the ceilidhing - a little bit of home!
+<p>It's been a long time since my last update - I think there is a real shortage of this valuable resource called free time in Cambridge! It's been a turbulent year so far and we are only into March. Brexit has officially happened and for most people in the academic community it represents a big new challenege to collaborative and funding opportunities. Nonetheless, I have had a good time progressing with my work in Cambridge and my little grey cells are getting pushed every step of the way.
+</p>
+     
+<p>Outside of work, it started off as a fantastic Lent term. We celebrated Burn's night in College where I delivered the address to a haggis and joined in with the ceilidhing - a little bit of home! I've thrown myself into the role of Welfare Officer for the Christ's College MCR and organised a bunch of wellbeing events including craft events, rambling and ice cream parties. The town has really become a home and I love to walk the cobbled streets, exploring the incredible history woven through this place.  
+</p>
+     
+<p>
+I've been able to take advantage of the central location of Cambridge and travelled into London on a few occasions. When my family visited we toured the sights and went to see Hamilton the musical - an utterly amazing performance and ensemble. Then, with my girlfriend, I went to Alexandra Palace for the first time to see Cage the Elephant and enjoyed a wizard cocktail class in Hackney! 
+</p>
+     
+<p>
+Enough of Cage the Elephant and more about the obvious elephant in the room - indeed, above all else, 2020 has so far been the year Coronavirus moved into town. What started off as a distant illness in a wet market in Wuhan at the wane of 2019 has now exploded into a worldwide pandemic. Vocabulary such as 'social distancing' and 'self isolation' have now entered into the common vernacular. The UK has recently went into full lockdown and many of my closest pals here in Cambridge have had to escpae to their home countries with no warning. Borders have been shutting across the globe and uncertainty is the only certainty of the day. I've decided to remain in Cambridge and offer support where I can through my role as Welfare Officer. In fact the whole MCR committee has came tohether in this crisis and has created some fantatsic support mechanisms for those who have been unable to leave. Events have moved online and a virtual termcard is offering a bunch of ways for people to come together. A buddy system is also looking out for those who are showing symptoms and self isolating.
+</p>
+     
+<p>
+So how has my daily life changed? I'm working from home - I can access all my work remotely and skype call with my supervisor. We are not allowed any gatherings outisde our house and are only allowed out on essential business - whether that be shopping or our alloted slot for daily exercise. The police are able to enforce this and rightfully so! The news from across the world is showing this virus is capable of large loss of life. Epidemiological modelling suggested scary UK mortality numbers if nothing was to be done and even with strict controls the NHS will be put under extreme stress. Volunteers are going to be essential to bolstering our health system at this demanding time! 
+</p>
+     
+<p>
+My family back home are also all working from the house and whilst I would love to go back I think its best to keep my distance and limit the possible spread to grandparents. It's not yet known how long these restrictions will be in place, but until an exit strategy becomes apparent...it could be months. So what is the goal? Vaccines are hurriedly being developed by scientific teams across the world, but even they wont be available for mass production within at least a year. In the meantime, will so called Herd Immunity be a valid strategy? Slowly allowing the population to become infected in a controlled manner in order to develop a population immune response. Sacrificing some to save more in the longer term? Indeed, this crisis challeneges not only our healthcare system and lifestyle logistics. It fundamentally shakes up political discourse, the direction of world globalisation and poses long term ethical questions.
+</p>
+     
+<p>
+I don't think many of us expected to be living through such stange times. In many ways it forces us to reflect on the relative peace of the lifestyles we have grown accustomed to in the Western world. Not since war time has the UK seen such restrictions on our shores. This threat, is in that respect, somewhat a new world war; but one far more encouraging in terms of collective global spirit. Here countries and people have been pulling together...expcept when it comes to pasta or toilet roll. Let's see where the coming weeks and months take us but I'm hoping that everyone stays healthy and hopefully my next post will see a return to some sort of normality. 
 </p>
      
 </div>
