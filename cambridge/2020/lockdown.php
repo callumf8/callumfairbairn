1c505b1de34a1c51de9828a9ca3b84e4397bf588
Callum Fairbairn <callumf8@hotmail.co.uk>
2020-05-10 20:29:12 +0100

    Update lockdown.php

--- cambridge/2020/lockdown.php
+++ cambridge/2020/lockdown.php
@@ -29,6 +29,19 @@ include_once($path);
 </a>
      
 <p>
+So guaging by the tally marks on our lockdown board, the university closed it's doors 47 days ago and they seem far from reopening. Stay at Home save Lives has been the motto guiding us through the past couple of months and without such stringent measures the situation would have been catastrophic. As things stand, the UK has the second highest number of Covid-19 deaths in the world - recently topping 30,000. This is a devastating number but far less than the projected figure of half a million if no measures were put in place.
+</p>
+     
+<p> 
+My life in Cambridge has been a strangely, peaceful existence. The new normal blends week into weekend but I have tried to maintain a rythmn. This usually involves a standard working day from 9 till 5 and then some additional reading in the evening. The MCR committee has continued to be an active source of activities and various schemes exist to support students during lockdown. I think we are very fortnuate to be in a college which listens to the concerns of students and has productive conversations about how best to proceed.
+</p>
+     
+<p>
+There are far fewer social distractions which has distilled a clarity of mind which is quite refreshing. I take my daily walk to the college Fellows garden which is by far my favourite place in Cambridge. Somehow, being within the college walls makes the distressing news of the outside world shrink away and it is instead replaced by the beautiful sights and sounds of spring. I've enjoyed documenting this springtime vibrance by turning my hand to video-making and unleashing some questionable creativity through some poetry. I have also discovered a fox family who have made their home underneath the shed nearby Milton's Mulberry tree. There are 5 cubs who are all identical balls of fluff. Mum is quite bold and used to humans, but the cubs are still a confused by my camera. They must be loving the lockdown as they have the garden (and the squirrels) all to themselves! 
+</p>
+     
+<p>
+Of course I am missing my family, friends and girlfriend and can't wait for the restrictions to be eased when it is sensible to do so. Despite this, I also am treasuring this unique moment in time. I hope as a socitey that we can carry forward the positives when this is all over - the importance for self-reflection and recognising the value of things we used to take for granted. The great vastness of nature and the adventurous possibilities await us again soon, but we will return with enhanced appreciation for the spaces that must be preserved. A simple breath of fresh air, the breeze rustling through leaves and the evening sun reflecting off the river. The awareness of these simple pleasures has been a revelation I hope we can all remember.  
 </p>
      
      
